b231f5d99bb0d01d64403ca8076c18cd9bebb383
rohitvijayvargia <88153364+rohitvijayvargia@users.noreply.github.com>
2021-07-30 00:45:19 +0800

    Add files via upload

---
+++ src/main/java/rv/FirstProject/App.java
@@ -0,0 +1,13 @@
+package rv.FirstProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
